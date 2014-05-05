1e60deb86a31d10f2018ad19e3e0d57bf86d4f9a
Cristi Draghici <cristi@draghici.net>
2014-05-05 17:46:09 +0300

    http.php updates

--- system/http.php
+++ system/http.php
@@ -81,4 +81,102 @@ class http
         return false;
     }
 }
+
+
+class http2
+{
+    public $config   = array();
+    public $request  = array();
+    public $response = array();
+    
+    private $socket;
+    
+    /** Init the script */
+    public function http()
+    {
+        $this->config('timeout', 30);
+        $this->config('content_type', 'application/xml');
+        $this->config('agent', 'PHP Starfish HTTP Client');
+    }
+    // Shortway for setting GET requests request configuration
+    public function url($string)
+    {
+        return true;
+    }
+    public function page($host, $port=80, $method="GET", $path='/', $data=array())
+    {
+        $this->request('host', $host);
+        $this->request('port', (int)$port);
+        $this->request('method', strtoupper($method));
+        $this->request('path', $path);
+        $this->request('data', $data);
+        
+        return true;
+    }
+    
+    // Configuration
+    public function config($name, $value)
+    {
+        $this->config[$name] = $value;
+        return true;
+    }
+    public function request($name, $value)
+    {
+        $this->request[$name] = $value;
+        return true;
+    }
+    
+    public function exec()
+    {
+        if ($this->connect())
+        {
+            @fwrite($this->socket,
+                $this->request['method']." ".$this->request['path']." HTTP/1.0\r\n".
+                "Host:".$this->request['host'].":".$this->request['port']."\r\n".
+                "User-Agent: ".$this->config['agent']."\r\n".
+                "Content-Type: ".$this->config['content_type']."\r\n".
+                "Content-Length: ".strlen( $this->request['data'] ).
+                "\r\n".
+                "\r\n".$this->request['data'].
+                "\r\n"
+            );
+            
+            while(!@feof($this->socket))
+            {
+                $this->response['result'] .= @fgets($this->socket, 2048);
+            }
+            $this->disconnect();
+            
+            $pos = strpos($this->response['result'], "\r\n\r\n") + 4;
+            $this->response['headers'] = substr($this->response['result'], 0, $pos);
+            $this->response['body']    = substr($this->response['result'], $pos);
+            $this->response['status']  = true;
+            
+            return $this->response['body'];
+        }
+        
+        $this->response['status'] = false;
+        return false;
+    }
+    
+    /** Internal functionality */
+    private function connect()
+    {
+        $this->socket = @fsockopen($this->request['host'], $this->request['port'], $this->response['no'], $this->response['str'], $this->config['timeout'] );
+        if (!$this->socket)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    private function disconnect()
+    {
+        @fclose($this->socket);
+        
+        return true;
+    }
+}
 ?>

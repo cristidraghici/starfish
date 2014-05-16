bd0b504ff5b35615387dbec525e598d0ce4c3fa1
Cristi Draghici <cristi@draghici.net>
2014-05-16 11:51:05 +0300

    MVC and cleanup

--- system/mvc.php
+++ system/mvc.php
@@ -3,6 +3,19 @@ if (!isset($path)) { die(); }
 
 trait mvc
 {
+	/** Model/View/Controller Applications */
+	public static function c($name)
+	{
+		return starfish::obj('controller-'.$name);
+	}
+	public static function m($name)
+	{
+		return starfish::obj('model-'$name);
+	}
+	public static function v($name, $data=array())
+	{
+		return starfish::obj('tpl')->view($name, $data);
+	}
 }
 
 ?>

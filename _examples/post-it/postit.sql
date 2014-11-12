76fc97dc82192acec82eab28c0e7048f6fc0d826
metalnot <razvanviorel@yahoo.com>
2014-11-13 00:56:32 +0200

    Create postit
    dump postit database

---
+++ _examples/post-it/postit.sql
@@ -0,0 +1,47 @@
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for postit
+CREATE DATABASE IF NOT EXISTS `postit` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `postit`;
+
+
+-- Dumping structure for table postit.categories
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `owner_id` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table postit.notes
+CREATE TABLE IF NOT EXISTS `notes` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `content` text COLLATE utf8_bin,
+  `category_id` int(20) unsigned DEFAULT NULL,
+  `owner_id` int(20) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table postit.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `pass` varchar(64) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
